7000dfb04eb7ac0303b705c37bc3e1527a653f5b
YvesShen <KevinYeti@gmail.com>
2021-05-24 13:40:08 +0800

    修复STDEV.P函数计算错误

--- main/SS/Formula/Functions/STDEV_P.cs
+++ main/SS/Formula/Functions/STDEV_P.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static NPOI.SS.Formula.Functions.AggregateFunction;
 
 namespace NPOI.SS.Formula.Functions
 {
@@ -11,7 +12,14 @@ namespace NPOI.SS.Formula.Functions
 
         public ValueEval Evaluate(ValueEval[] args, OperationEvaluationContext ec)
         {
-            return args[0];
+            var values = ValueCollector.CollectValues(args);
+
+            if (values.Length < 1)
+            {
+                throw new EvaluationException(ErrorEval.DIV_ZERO);
+            }
+
+            return new NumberEval(StatsLib.stdevp(values));
         }
     }
 }
